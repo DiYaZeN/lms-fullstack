fa9b46c61c1a73e4db9bbafea970b64e93e1cb4b
DiYaZeN <minolamarakoon@gmail.com>
2023-10-07 18:21:40 +0530

    feat:auth

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <p className="text-3xl">hi</p>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 }
